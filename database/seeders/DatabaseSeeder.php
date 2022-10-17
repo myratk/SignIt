fb37b77c0948871dff74916aeb8bbdae8f0cd352
Myra Khokhar <myrakhokhar73@gmail.com>
2022-10-17 10:20:36 +0100

    Add user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,13 +5,15 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Database\Seeders\VisitorSeeder;
+use Database\Seeders\UserSeeder;
 
 class DatabaseSeeder extends Seeder
 {
     public function run()
     {
         $this->call([
-            VisitorSeeder::class
+            VisitorSeeder::class,
+            UserSeeder::class
         ]);
     }
 }
